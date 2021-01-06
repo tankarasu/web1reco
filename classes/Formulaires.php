5230d2a470cdaf94c117d168a0012bb726b4ed25
Tan KARASU <karasutan@gmail.com>
2021-01-06 06:39:36 +0100

    inclusion de bootstrap, d'un dossier class avec un objet formulaire et d'un fichier css

---
+++ classes/Formulaires.php
@@ -0,0 +1,15 @@
+<?php
+class Formulaire
+{
+    
+// public function __construct()
+//     {
+ 
+//     }
+
+public function renderTypeOne() // TODO change the name of the function
+    {
+    echo  '<form></form>';
+    }
+
+}?>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+// includes 
+require './classes/Formulaires.php';
+// instanciate class
+$formulaire = new Formulaire();// TODO change the name for the first formulaire
+
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Drive Service</title>
+    <link href="./assets/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+<body>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
